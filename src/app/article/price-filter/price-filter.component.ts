6ed71fd2ddc33ea69dfc40395ab7f37b3043d8ad
Ahmed <ahmedbhs123@gmail.com>
2018-12-29 18:17:26 +0100

    Add fix prices

--- src/app/article/price-filter/price-filter.component.ts
+++ src/app/article/price-filter/price-filter.component.ts
@@ -2,46 +2,44 @@ import {Component, EventEmitter, Input, OnChanges, Output} from '@angular/core';
 import {LabelType, Options} from 'ng5-slider';
 
 @Component({
-  selector: 'app-price-filter',
-  templateUrl: './price-filter.component.html',
-  styleUrls: ['./price-filter.component.css']
+    selector: 'app-price-filter',
+    templateUrl: './price-filter.component.html',
+    styleUrls: ['./price-filter.component.css']
 })
 export class PriceFilterComponent implements OnChanges {
-    @Input() floor ;
+    @Input() floor;
     @Input() ceil;
     @Output() eventEmitterFilter = new EventEmitter<{}>();
     min: any;
     max: any;
 
     options: Options;
+
     ngOnChanges() {
-        if ( this.floor && this.ceil) {
-          console.log( isNaN(this.max));
-           if (isNaN(this.max) && isNaN(this.min)) {
-            this.max = Number(this.ceil.toFixed(0)) || 0;
+        if (this.floor && this.ceil) {
 
-            this.min = Number(this.floor.toFixed(0)) || 0;
-            console.log(this.min);
-            this.options = {
-              floor: this.floor,
-              ceil: this.ceil,
-              showSelectionBar: true,
-              selectionBarGradient: {
-                from: 'white',
-                to: '#007bff'
-              },
-              showOuterSelectionBars: true,
-              translate: (value: number, label: LabelType): string => {
-                return value + '€';
-              }
-          };
-           }
+            if (isNaN(this.max) && isNaN(this.min)) {
+                this.max = Number(this.ceil.toFixed(0)) || 0;
+                this.min = Number(this.floor.toFixed(0)) || 0;
 
-
+                this.options = {
+                    floor: this.floor,
+                    ceil: this.ceil,
+                    showSelectionBar: true,
+                    selectionBarGradient: {
+                        from: 'white',
+                        to: '#007bff'
+                    },
+                    showOuterSelectionBars: true,
+                    translate: (value: number, label: LabelType): string => {
+                        return value + '€';
+                    }
+                };
+            }
         }
     }
 
- filter() {
-    this.eventEmitterFilter.emit({'ceil' : this.max, 'floor' : this.min});
- }
+    filter() {
+        this.eventEmitterFilter.emit({'ceil': this.max, 'floor': this.min});
+    }
 }
